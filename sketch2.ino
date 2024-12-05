9c7b2861b0a95ce50327a4ea0f713e7bdd53df6d
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-12-05 13:08:40 +0100

    sketch2.ino

---
+++ sketch2.ino
@@ -0,0 +1,32 @@
+# include <Server.h>
+
+Servo servo;
+int lightSensor1 = 9;
+int lightSensor2 = 10;
+
+void setup() {
+	servo.attach(10);
+	pinMode(lightSensor1, INPUT);
+	pinMode(lightSensor2, INPUT);
+}
+
+void loop() {
+	turn();
+}
+
+void turn() {
+	int light2 = digitalRead(lightSensor2);
+	int light1 = digitalRead(lightSensor1);
+	
+	if (light1 == LOW) {
+		servo.write(90);
+	}
+
+	if (light2 == LOW) {
+		servo.write(-90);
+	}
+
+	if (light1 == LOW && light2 == LOW) {
+		// Do nothing or later print out value
+	}
+}
