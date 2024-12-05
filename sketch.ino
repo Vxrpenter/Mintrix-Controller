7cb7fafd9e2c59a0322436f9316ed9fb80e46caa
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-12-05 12:49:06 +0100

    sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,18 @@
+# include <Server.h>
+
+int currentAngle = 0;
+int lightSensorPin = 9;
+
+void setup() {
+	pinMode(lightSensorPin, INPUT);
+}
+
+void loop() {
+	if () {
+	
+	}
+}
+
+void turn() {
+	
+}
