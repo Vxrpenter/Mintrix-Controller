3a3d7d2f1135c8b1ae0ae460ddc9df718d9b6ec8
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-12-05 12:57:07 +0100

    sketch.ino update

--- sketch.ino
+++ sketch.ino
@@ -1,18 +1,27 @@
 # include <Server.h>
 
+Servo servo;
 int currentAngle = 0;
 int lightSensorPin = 9;
 
 void setup() {
+	servo.attach(10);
 	pinMode(lightSensorPin, INPUT);
 }
 
 void loop() {
-	if () {
-	
+	int sensor = digitalRead(lightSensorPin);
+	if (sensor == LOW) {
+		if (currentAngle != 90) {
+			turn();
+		} else {
+			currentAngle = 0;
+		 turn();
+		}
 	}
 }
 
 void turn() {
-	
+	currentAngle++;
+	servo.write(currentAngle);
 }
