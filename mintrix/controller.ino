8577d0b948318b1d7d343c4fb0c9246574b9957c
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-02-06 13:07:07 +0100

    Update controller.ino

--- mintrix/controller.ino
+++ mintrix/controller.ino
@@ -12,6 +12,15 @@ int calculateLedPosition(int row, int led) {
   return returnValue;
 }
 
+bool integerState(int integer) {
+  Serial.println(integer%2);
+  if (integer%2 > 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 void FillCompletely(CRGB color) {
   for(int x=0; x < 81; x++) {
     Mintrix.leds[x] = color;
@@ -39,6 +48,38 @@ void BlinkLED(int row, int led, CRGB color, int recursions, int customDelay = 50
     delay(customDelay);
     Mintrix.leds[exactLed] = CRGB::Black;
     Mintrix.show();
+    delay(customDelay);
+  }
+}
+
+void MartixLights(CRGB colorOne, CRGB colorTwo, int recursions = 10, int customDelay = 500) {
+  bool currentState = true;
+  
+  for(int x=0; x < recursions; x++) {
+    CRGB currentColor = CRGB::Black;
+
+    if (currentState = true) {
+      currentColor = colorOne;
+    } else {
+      currentColor = colorTwo;
+    }
+    
+    for(int y=0; y < 81; x++) {
+      bool currentIntegerState = integerState(y);
+      Serial.println(currentIntegerState);
+
+      if (currentIntegerState == currentState) {
+        Mintrix.leds[y] = currentColor;
+      }
+    }
+    Mintrix.show();
+
+    if (currentState = true) {
+      currentState = false;
+    } else {
+      currentState = true;
+    }
+    delay(customDelay);
   }
 }
 
@@ -47,10 +88,11 @@ void BlinkLED(int row, int led, CRGB color, int recursions, int customDelay = 50
 //
 
 void setup() {
+  Serial.begin(9600);
   Mintrix.clear();
   Mintrix.show();
 }
 
 void loop() {
-  BlinkLED(1, 5, CRGB::Red, 50);
+  MartixLights(CRGB::Blue, CRGB::Red);
 }
