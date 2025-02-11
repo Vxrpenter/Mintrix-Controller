0a7a5757f3b30d8b07b204231e71a991314122ce
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-02-11 11:14:33 +0100

    Update and rename controller.ino to MintrixController.ino

---
+++ mintrix/MintrixController.ino
@@ -0,0 +1,17 @@
+#include<Mintrix.h>
+#include "DisplayProtocols.h"
+#include "PictureDisplayScipt.h"
+
+//
+// Loop and Setup Methods
+//
+
+void setup() {
+  Serial.begin(9600);
+  Mintrix.clear();
+  Mintrix.show();
+}
+
+void loop() {
+  martixLights(CRGB::Blue, CRGB::Red);
+}
