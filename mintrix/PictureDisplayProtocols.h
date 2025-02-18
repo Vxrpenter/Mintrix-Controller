aa9611f29fc05c9cd815338eed5e5449fc2aaf08
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-02-18 11:21:26 +0100

    Update PictureDisplayProtocols.h

--- mintrix/PictureDisplayProtocols.h
+++ mintrix/PictureDisplayProtocols.h
@@ -1,1 +1,22 @@
+CRGB picture[9][9] = {
+  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
+  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
+  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
+  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
+  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
+  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
+  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
+  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
+  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}
+};
 
+void displayPicture() {
+  for (int i = 0; i < 9; i++) {
+    for (int x = 0; i < 9; x++) {
+      int currentPixel = calculateProgramLedPosition(i, x);
+      
+      Mintrix.leds[currentPixel] = picture[i][x];
+      Mintrix.show();
+    }
+  }
+}
