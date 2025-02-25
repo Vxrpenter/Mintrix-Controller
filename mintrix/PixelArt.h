a1aab3daeda6103e0b3f2384cee802cff68dc95d
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-02-25 11:23:56 +0100

    Create PixelArt.h

---
+++ mintrix/PixelArt.h
@@ -0,0 +1,51 @@
+//
+// POKEMON
+//
+
+CRGB glumanda[9][9] = {
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Red}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Yellow},
+  {CRGB::Black, CRGB::Orange, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black}
+};
+
+CRGB pokeball[9][9] = {
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Black}, 
+  {CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::White, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::White, CRGB::White, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::White, CRGB::White, CRGB::Black},
+  {CRGB::Black, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+};
+
+CRGB pikachu[9][9] = {
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Red, CRGB::Yellow, CRGB::Yellow, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Brown, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+};
+
+CRGB blank[9][9] = {
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+};
