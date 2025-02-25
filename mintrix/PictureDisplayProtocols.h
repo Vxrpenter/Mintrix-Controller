9ef4e8c9a33508697af9cadab8528a392f031a8e
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-02-25 11:23:35 +0100

    Update PictureDisplayProtocols.h

--- mintrix/PictureDisplayProtocols.h
+++ mintrix/PictureDisplayProtocols.h
@@ -1,22 +1,10 @@
-CRGB picture[9][9] = {
-  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
-  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
-  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
-  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
-  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
-  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
-  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}, 
-  {CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green},
-  {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red}
-};
-
 void displayPicture() {
   for (int i = 0; i < 9; i++) {
-    for (int x = 0; i < 9; x++) {
+    for (int x = 0; x < 9; x++) {
       int currentPixel = calculateProgramLedPosition(i, x);
-      
-      Mintrix.leds[currentPixel] = picture[i][x];
-      Mintrix.show();
+      Mintrix.leds[currentPixel] = pikachu[i][x];
     }
   }
+  Mintrix.show();
+  delay(1000);
 }
