dc38992269e43a6413b3405249c85c7896826d6a
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-03-25 10:52:11 +0100

    Update PictureDisplayProtocols.h

--- mintrix/PictureDisplayProtocols.h
+++ mintrix/PictureDisplayProtocols.h
@@ -1,10 +1,30 @@
-void displayPicture() {
+void displayPixels(CRGB picture[9][9]) {
   for (int i = 0; i < 9; i++) {
     for (int x = 0; x < 9; x++) {
       int currentPixel = calculateProgramLedPosition(i, x);
-      Mintrix.leds[currentPixel] = pikachu[i][x];
+      Mintrix.leds[currentPixel] = picture[i][x];
     }
   }
   Mintrix.show();
   delay(1000);
+}
+
+void displayPicture(PixelArtType pixelArt) {
+  switch(pixelArt) {
+    case GLUMANDA:
+      displayPixels(glumanda);
+      break;
+      
+    case POKEBALL:
+      displayPixels(pokeball);
+      break;
+      
+    case PIKACHU:
+      displayPixels(pikachu);
+      break;
+      
+    case BLANK:
+      displayPixels(blank);
+      break;
+  }
 }
