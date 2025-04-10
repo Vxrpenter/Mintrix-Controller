bb27884545a62ae91ab8ab6123c8e8a806a3f6ee
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-04-10 13:04:28 +0200

    Create Numbers.h

---
+++ mintrix/Numbers.h
@@ -0,0 +1,62 @@
+//
+// Single Numbers
+//
+
+CRGB[9][9] singleOne(CRGB color) {
+  return {
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, color, color, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+  };
+}
+
+CRGB[9][9] singleTwo(CRGB color) {
+  return {
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, color, color, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, color, color, color, color, color, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+  };
+}
+
+CRGB[9][9] singleThree(CRGB color) {
+  return {
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, color, color, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, color, color, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black, CRGB::Black, color, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, color, color, color, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+  };
+}
+
+// For further Numbers https://www.vecteezy.com/vector-art/25735659-numbers-in-pixel-style-numbers-from-zero-to-nine-vector-scalable-graphics
+CRGB[9][9] singleFour(CRGB color) {
+  return {
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+  };
+}
+
+// Multidigit Numbers
