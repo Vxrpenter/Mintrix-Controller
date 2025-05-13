badeac6dc67b6d928e05eeeba75e72eee7ddd02d
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-05-13 11:03:06 +0200

    Create glumanda.h

---
+++ mintrix/pictures/glumanda.h
@@ -0,0 +1,11 @@
+CRGB glumanda[9][9] = {
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Red}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Yellow},
+  {CRGB::Black, CRGB::Orange, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black}
+};
