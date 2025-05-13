bd0b355225a06b27bc15faa24f600211be11541c
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-05-13 11:03:40 +0200

    Create pikachu.h

---
+++ mintrix/pictures/pikachu.h
@@ -0,0 +1,11 @@
+CRGB pikachu[9][9] = {
+  {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Red, CRGB::Yellow, CRGB::Yellow, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Black, CRGB::Black}, 
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Brown, CRGB::Black, CRGB::Black, CRGB::Black},
+  {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+};
