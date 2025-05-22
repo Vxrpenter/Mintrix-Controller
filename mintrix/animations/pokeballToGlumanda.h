7ff2f9cff76380b25310879999d3ccff26b3a7f5
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-05-22 12:50:15 +0200

    Create pokeballToGlumanda.h

---
+++ mintrix/animations/pokeballToGlumanda.h
@@ -0,0 +1,24 @@
+CRGB pokeballToGlumanda[][9][9] = {
+  {
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Red}, 
+    {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Yellow},
+    {CRGB::Black, CRGB::Orange, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Orange, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Yellow, CRGB::Yellow, CRGB::Orange, CRGB::Orange, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Black, CRGB::Black}
+  },
+  {
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Black}, 
+    {CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Red, CRGB::Red, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::White, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}, 
+    {CRGB::Black, CRGB::White, CRGB::White, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::White, CRGB::White, CRGB::Black},
+    {CRGB::Black, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::Black}, 
+    {CRGB::Black, CRGB::Black, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::White, CRGB::Black, CRGB::Black},
+    {CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black}
+  }
+};
