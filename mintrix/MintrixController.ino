6baafe0a65c4fe4ef5113d6afa685023add76b24
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-05-22 12:58:47 +0200

    Update MintrixController.ino

--- mintrix/MintrixController.ino
+++ mintrix/MintrixController.ino
@@ -1,13 +1,10 @@
 #include <Mintrix.h>
-#include "ESP8266WebServer.h"
-#include "ESP8266WiFi.h"
 
 // Manifest Includes
 #include "pictures/manifest.h"
 #include "animations/manifest.h"
 
 // Included Classes
-#include "WebHandler.h"
 #include "Enums.h"
 #include "Utility.h"
 #include "DisplayProtocols.h"
@@ -22,14 +19,8 @@ void setup() {
   Serial.begin(9600);
   Mintrix.clear();
   Mintrix.show();
-
-  // Starting web and wifi stuff
-  Mintrix.staticWiFi("MintrixController");
-  Mintrix.startWeb();
-  Mintrix.setWebPage(testPage);
 }
 
 void loop() {
-  Mintrix.handleWeb();
   displayAnimation(2, 2, animation);
 }
