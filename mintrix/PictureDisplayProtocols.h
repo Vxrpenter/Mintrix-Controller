e55245000acd218d68d986fc79ecc3ca14c44eaf
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-06-17 10:31:10 +0200

    Update PictureDisplayProtocols.h

--- mintrix/PictureDisplayProtocols.h
+++ mintrix/PictureDisplayProtocols.h
@@ -1,4 +1,5 @@
 void displayFrame(CRGB picture[9][9]) {
+  Mintrix.clear();
   for (int i = 0; i < 9; i++) {
     for (int x = 0; x < 9; x++) {
       int currentPixel = calculateProgramLedPosition(i, x);
