db13df937c93e3f7bdf9832c66132e6ca501f74f
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2025-06-17 10:30:28 +0200

    Update AnimationDisplayProtocols.h

--- mintrix/AnimationDisplayProtocols.h
+++ mintrix/AnimationDisplayProtocols.h
@@ -1,4 +1,5 @@
 void displayAnimation(int frames, int framerate, CRGB animation[][9][9]) {
+  Mintrix.clear();
   for (int i = 0; i < frames; i++) {
     displayFrame(animation[i]);
     delay(framerate);
